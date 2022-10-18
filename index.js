4bc227227d49ff44679977fdc4d37b2c6e65d018
arjunrvan <arjunrvan@gmail.com>
2022-10-18 09:28:38 +0800

    third api - get with query

--- index.js
+++ index.js
@@ -6,4 +6,16 @@ app.get("/hello", (req, res) => {
   res.send("Hello World");
 });
 
+// Second API - Get with params
+app.get("/user/:name", (req, res) => {
+  const name = req.params.name;
+  res.send(`Hello ${name}`);
+});
+
+// Third API - Get with query
+app.get("/person", (req, res) => {
+  const name = req.query.name;
+  res.send(`Hello ${name}`);
+});
+
 app.listen(3000, () => console.log("Listening on port 3000"));
